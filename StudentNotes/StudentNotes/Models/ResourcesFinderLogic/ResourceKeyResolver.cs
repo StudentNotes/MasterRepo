8866f48ff3eb47a179b8f036a03169989da311a0
robson081192 <robson081192@gmail.com>
2015-10-07 01:15:32 +0200

    HomePage content moved to resource file - 100% done!

--- StudentNotes/StudentNotes/Models/ResourcesFinderLogic/ResourceKeyResolver.cs
+++ StudentNotes/StudentNotes/Models/ResourcesFinderLogic/ResourceKeyResolver.cs
@@ -59,5 +59,252 @@ namespace StudentNotesWeb.Models.ResourcesFinderLogic
         {
             get { return ResourceFinder.GetResource("AccordionFeaturesContentHeader"); }
         }
+
+        public static string ConstStoring
+        {
+            get { return ResourceFinder.GetResource("ConstStoring"); }
+        }
+        public static string ConstSharing
+        {
+            get { return ResourceFinder.GetResource("ConstSharing"); }
+        }
+        public static string AccordionFeaturesFirstContent
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesFirstContent"); }
+        }
+        public static string ConstYou
+        {
+            get { return ResourceFinder.GetResource("ConstYou"); }
+        }
+        public static string AccordionFeaturesFirstContentCd
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesFirstContentCd"); }
+        }
+        public static string AccordionFeaturesSecondContent
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesSecondContent"); }
+        }
+        public static string ConstTagging
+        {
+            get { return ResourceFinder.GetResource("ConstTagging"); }
+        }
+        public static string ConstSearching
+        {
+            get { return ResourceFinder.GetResource("ConstSearching"); }
+        }
+        public static string AccordionFeaturesThirdContent
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesThirdContent"); }
+        }
+        public static string AccordionFeaturesThirdContentCd
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesThirdContentCd"); }
+        }
+        public static string ConstManagement
+        {
+            get { return ResourceFinder.GetResource("ConstManagement"); }
+        }
+        public static string AccordionFeaturesFourthContent
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesFourthContent"); }
+        }
+        public static string ConstYours
+        {
+            get { return ResourceFinder.GetResource("ConstYours"); }
+        }
+        public static string AccordionFeaturesFourthContentCd
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesFourthContentCd"); }
+        }
+        public static string ConstDownloading
+        {
+            get { return ResourceFinder.GetResource("ConstDownloading"); }
+        }
+        public static string AccordionFeaturesFifthContent
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesFifthContent"); }
+        }
+
+        public static string AccordionFeaturesContentBody
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesContentBody"); }
+        }
+        public static string AccordionFeaturesContentBodyCd
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesContentBodyCd"); }
+        }
+        public static string AccordionFeaturesContentBodyCd_1
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesContentBodyCd_1"); }
+        }
+        public static string AccordionFeaturesContentBodyCd_2
+        {
+            get { return ResourceFinder.GetResource("AccordionFeaturesContentBodyCd_2"); }
+        }
+        public static string ConstSignUp
+        {
+            get { return ResourceFinder.GetResource("ConstSignUp"); }
+        }
+        public static string AccordionExtensionsTitle
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsTitle"); }
+        }
+        public static string ConstService
+        {
+            get { return ResourceFinder.GetResource("ConstService"); }
+        }
+        public static string AccordionExtensionsContentHeader
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsContentHeader"); }
+        }
+        public static string AccordionExtensionsContentHeaderCd
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsContentHeaderCd"); }
+        }
+        public static string AccordionExtensionsContentHeaderCd_1
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsContentHeaderCd_1"); }
+        }
+
+        public static string AccordionExtensionsFirstContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsFirstContentType"); }
+        }
+        public static string AccordionExtensionsFirstContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsFirstContentDesc"); }
+        }
+        public static string AccordionExtensionsSecondContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsSecondContentType"); }
+        }
+        public static string AccordionExtensionsThirdContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsThirdContentType"); }
+        }
+        public static string AccordionExtensionsFourthContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsFourthContentType"); }
+        }
+        public static string AccordionExtensionsFifthContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsFifthContentType"); }
+        }
+        public static string AccordionExtensionsSixthContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsSixthContentType"); }
+        }
+        public static string AccordionExtensionsSeventhContentType
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsSeventhContentType"); }
+        }
+        public static string AccordionExtensionsSecondContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsSecondContentDesc"); }
+        }
+        public static string AccordionExtensionsThirdContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsThirdContentDesc"); }
+        }
+        public static string AccordionExtensionsFourthContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsFourthContentDesc"); }
+        }
+        public static string AccordionExtensionsFifthContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsFifthContentDesc"); }
+        }
+        public static string AccordionExtensionsSixthContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsSixthContentDesc"); }
+        }
+        public static string AccordionExtensionsSeventhContentDesc
+        {
+            get { return ResourceFinder.GetResource("AccordionExtensionsSeventhContentDesc"); }
+        }
+        public static string AccordionRulesTitle
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesTitle"); }
+        }
+        public static string AccordionRulesContentHeader
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentHeader"); }
+        }
+        public static string AccordionRulesContentHeaderCd
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentHeaderCd"); }
+        }
+        public static string AccordionRulesContentFirstRule
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentFirstRule"); }
+        }
+        public static string AccordionRulesContentSecondRule
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentSecondRule"); }
+        }
+        public static string AccordionRulesContentThirdRule
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentThirdRule"); }
+        }
+        public static string AccordionRulesContentFourthRule
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentFourthRule"); }
+        }
+        public static string AccordionRulesContentBody
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentBody"); }
+        }
+        public static string AccordionRulesContentBodyCd
+        {
+            get { return ResourceFinder.GetResource("AccordionRulesContentBodyCd"); }
+        }
+        public static string JumbotronAboutUserGreeting
+        {
+            get { return ResourceFinder.GetResource("JumbotronAboutUserGreeting"); }
+        }
+        public static string JumbotronAboutUserContent_1
+        {
+            get { return ResourceFinder.GetResource("JumbotronAboutUserContent_1"); }
+        }
+        public static string JumbotronAboutUserContent_2
+        {
+            get { return ResourceFinder.GetResource("JumbotronAboutUserContent_2"); }
+        }
+        public static string JumbotronAboutUserContent_3
+        {
+            get { return ResourceFinder.GetResource("JumbotronAboutUserContent_3"); }
+        }
+        public static string ConstSignUpInService
+        {
+            get { return ResourceFinder.GetResource("ConstSignUpInService"); }
+        }
+        public static string SignUpFormLabel_1
+        {
+            get { return ResourceFinder.GetResource("SignUpFormLabel_1"); }
+        }
+        public static string SignUpFormLabel_2
+        {
+            get { return ResourceFinder.GetResource("SignUpFormLabel_2"); }
+        }
+        public static string SignUpFormLabel_3
+        {
+            get { return ResourceFinder.GetResource("SignUpFormLabel_3"); }
+        }
+        public static string HomePageNavBarFormGroupConfirmPassword
+        {
+            get { return ResourceFinder.GetResource("HomePageNavBarFormGroupConfirmPassword"); }
+        }
+        public static string SignUpFormLabel_4
+        {
+            get { return ResourceFinder.GetResource("SignUpFormLabel_4"); }
+        }
+        public static string SignUpFormLabel_4Cd
+        {
+            get { return ResourceFinder.GetResource("SignUpFormLabel_4Cd"); }
+        }
+        public static string ConstTermsOfUse
+        {
+            get { return ResourceFinder.GetResource("ConstTermsOfUse"); }
+        }
     }
 }
