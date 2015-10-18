c738e4aa78ab955991ecbda254e8ea29d62fbd22
robson081192 <robson081192@gmail.com>
2015-10-18 15:41:14 +0200

    Namespace refactoring.

--- StudentNotes/StudentNotesLogic/Properties/AssemblyInfo.cs
+++ StudentNotes/StudentNotesLogic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StudentNotesDal")]
+[assembly: AssemblyTitle("StudentNotesLogic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StudentNotesDal")]
+[assembly: AssemblyProduct("StudentNotesLogic")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
